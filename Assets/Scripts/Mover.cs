0deb3d694b8c4761be1e1df6294d954c26675f5f
M. Ahsan Nasir <ahsan.mzd01@gmail.com>
2026-09-10 03:14:51 +0500

    Remove TutorialInfo assets and add new wall material
    - Deleted Layout.wlt and its associated meta file, along with Readme.cs and its meta file, and StyleSheets related files.
    - Introduced a new material asset (wallMat.mat) with its corresponding meta file.
    - Updated manifest.json to include the Cinemachine package.
    - Updated packages-lock.json to reflect the addition of new dependencies.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-
+    [SerializeField] float  moveSpeed = 5f;
     
 
     void Start()
@@ -13,9 +13,9 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
-        float zValue = Input.GetAxis("Vertical");
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
         transform.Translate(xValue, yValue, zValue);
     }
